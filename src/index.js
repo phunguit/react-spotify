18d7e711a6a8ba88b838c317b527f515ecd29a1a
DESKTOP-HLKGEMR\Admin <phungthaouit@gmail.com>
2018-04-25 22:06:29 +0700

    - Add reducers.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers/index';
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+<Provider store={ store }>
+    <App />
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
